d66d1f5ac997bdf3912c93159496a1868fa901fc
Stephen Huang <stephenhuang@augmentum.com.cn>
2015-05-21 16:06:41 +0800

    check in

--- src/WebContent/static/js/controllers/index.js
+++ src/WebContent/static/js/controllers/index.js
@@ -1,5 +1,9 @@
 define(['modules/controller', 'jQuery', "services/user"], function(controller, $) {
-  controller.controller('indexCtrl', ['$scope', '$state', '$stateParams', function($scope, $state, $stateParams) {
+  controller.controller('indexCtrl', ['$scope', '$state', '$stateParams', 'userService', function($scope, $state, $stateParams, userService) {
+    
+    userService.logout().success(function(data) {
+      var result = data.data;
+    });
     var user = {};
     user.userName = "Stephen";
     $scope.user = user;
--- src/WebContent/static/js/main/index.js
+++ src/WebContent/static/js/main/index.js
@@ -5,7 +5,7 @@ require.config({
     'jQuery': 'lib/jquery-1.10.2.min',
     'angular': 'lib/angular',
     'angular-resource': 'lib/angular-resource',
-    'angular-ui-router': 'lib/angular-ui-router.min',
+    'angular-ui-router': 'lib/angular-ui-router.min'
   },
   shim: {
     'jQuery': {exports: 'jQuery'},
